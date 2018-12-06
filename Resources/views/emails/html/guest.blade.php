a7fc5c888dcfca6417925d89a00a71af8da6a33f
sagsoz06 <designer@visualturk.net>
2018-12-06 23:50:49 +0300

    Fixed notification email for guest

--- Resources/views/emails/html/guest.blade.php
+++ Resources/views/emails/html/guest.blade.php
@@ -1,9 +1,9 @@
 <html>
     <body>
         <p>{{ trans('themes::contact.mail.dear') }} {{ $contact->first_name }} {{ $contact->last_name }}</p>
-        <p>{!! trans('themes::contact.mail.message') !!}</p>
-        <p style="color:red;">{!! trans('themes::contact.mail.date') !!} {{ \Carbon\Carbon::now()->format('d.m.Y H:i') }}</p>
-        <p style="color:red;">{!! trans('themes::contact.mail.ip') !!} {{ $contact->ip }}</p>
+        <p><strong>{!! trans('themes::contact.mail.message') !!}</strong>: {{ $contact->enquiry }}</p>
+        <p style="color:red;"><strong>{!! trans('themes::contact.mail.date') !!}</strong>: {{ \Carbon\Carbon::now()->format('d.m.Y H:i') }}</p>
+        <p style="color:red;"><strong>{!! trans('themes::contact.mail.ip') !!}</strong>: {{ $contact->ip }}</p>
         <hr>
         <p>{{ setting('theme::company-name') }}</p>
         <p>{{ setting('theme::address') }}</p>
